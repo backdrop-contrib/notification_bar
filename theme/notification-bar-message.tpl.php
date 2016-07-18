6f0f3eb7f5bdec6daaf783974003ef9fe7117e10
Zak Huber <zak@mjdinteractive.com>
2016-07-18 13:32:11 -0700

    2667320 - Optional show/hide for notification bar template

--- theme/notification-bar-message.tpl.php
+++ theme/notification-bar-message.tpl.php
@@ -1,6 +1,10 @@
 <div class="clearfix notification-bar-message notification-bar-<?php print $vars['type']; ?>">
-  <div class="content-left"><?php print $vars['message']['message_text_left']; ?></div>
-  <div class="content"><?php print $vars['message']['message_text']; ?></div>
+  <?php if (!empty($vars['message']['message_text_left'])): ?>
+    <div class="content-left"><?php print $vars['message']['message_text_left']; ?></div>
+  <?php endif; ?>
+  <?php if (!empty($vars['message']['message_text'])): ?>
+    <div class="content"><?php print $vars['message']['message_text']; ?></div>
+  <?php endif; ?>
   <div class="content-right">
     <?php if ($vars['message']['cta_text']): ?>
       <a class="btn" href="<?php print $vars['message']['cta_url']; ?>"><?php print $vars['message']['cta_text']; ?></a>
