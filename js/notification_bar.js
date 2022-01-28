f9e6157996bf63cb3196a007234753ba98f390a7
laryn <laryn@cedc.org>
2022-01-28 10:59:54 -0600

    Cleanup and basic functionality.

---
+++ js/notification_bar.js
@@ -0,0 +1,14 @@
+Backdrop.notification_bar = Backdrop.notification_bar || {};
+
+(function ($) {
+
+  Backdrop.notification_bar.initialize = function () {
+    // Move messages from closure to right after opening of body tag.
+    $("body > div.layout").prepend($("#notification-bar-messages"));
+  };
+
+  $(document).ready(function () {
+    Backdrop.notification_bar.initialize();
+  });
+
+})(jQuery);
--- theme/notification-bar-message.tpl.php
+++ theme/notification-bar-message.tpl.php
@@ -1,13 +1,17 @@
 <div class="clearfix notification-bar-message notification-bar-<?php print $vars['type']; ?>">
-  <?php if (!empty($vars['message']['message_text_left'])): ?>
-    <div class="content-left"><?php print $vars['message']['message_text_left']; ?></div>
+  <?php if (!empty($vars['message']['message_label'])): ?>
+    <div class="message-label">
+      <?php print $vars['message']['message_label']; ?>
+    </div>
   <?php endif; ?>
   <?php if (!empty($vars['message']['message_text'])): ?>
-    <div class="content"><?php print $vars['message']['message_text']; ?></div>
+    <div class="content">
+      <?php print $vars['message']['message_text']; ?>
+    </div>
   <?php endif; ?>
-  <div class="content-right">
-    <?php if ($vars['message']['cta_text']): ?>
-      <a class="btn" href="<?php print $vars['message']['cta_url']; ?>"><?php print $vars['message']['cta_text']; ?></a>
-    <?php endif; ?>
-  </div>
+  <?php if (!empty($vars['message']['cta_text'])): ?>
+    <div class="message-cta">
+      <a class="button" href="<?php print $vars['message']['cta_url']; ?>"><?php print $vars['message']['cta_text']; ?></a>
+    </div>
+  <?php endif; ?>
 </div>
